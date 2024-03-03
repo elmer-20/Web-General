8298789a58dd7697dec90a628428e1ae4e659b81
anghelogfp <anghelogfp@gmail.com>
2024-03-02 21:45:13 -0500

    add temp

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const countdownElement = document.getElementById('countdown');
+    const targetDate = new Date("March 23, 2024 06:00:00").getTime();
+
+    const countdownInterval = setInterval(function () {
+        const currentDate = new Date().getTime();
+        const difference = targetDate - currentDate;
+
+        const remainingTime = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+            minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+            seconds: Math.floor((difference % (1000 * 60)) / 1000)
+        };
+
+        const formattedTime = `${remainingTime.days}d ${remainingTime.hours}h ${remainingTime.minutes}m ${remainingTime.seconds}s`;
+        countdownElement.innerHTML = formattedTime;
+
+        if (difference < 0) {
+            clearInterval(countdownInterval);
+            countdownElement.innerHTML = '¡Llegó la Hora!';
+        }
+    }, 1000);
+});
+
+
+
 (function ($) {
     "use strict";
 
