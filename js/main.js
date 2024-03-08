82ffc9d7a881a5615fc67a552636574534eb981a
Anghelo Flores <anghelogfp@gmail.com>
2024-03-07 22:46:38 -0500

    add hora index

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const countdownElement = document.getElementById('countdown');
-    const targetDate = new Date("March 23, 2024 06:00:00").getTime();
+    const targetDate = new Date("March 08, 2024 08:00:00").getTime();
 
     const countdownInterval = setInterval(function () {
         const currentDate = new Date().getTime();
@@ -13,7 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
             seconds: Math.floor((difference % (1000 * 60)) / 1000)
         };
 
-        const formattedTime = `${remainingTime.days}d ${remainingTime.hours}h ${remainingTime.minutes}m ${remainingTime.seconds}s`;
+        //const formattedTime = `${remainingTime.days}d ${remainingTime.hours}h ${remainingTime.minutes}m ${remainingTime.seconds}s`;
+        //countdownElement.innerHTML = formattedTime;
+        const formattedTime = `${remainingTime.hours}h ${remainingTime.minutes}m ${remainingTime.seconds}s`;
         countdownElement.innerHTML = formattedTime;
 
         if (difference < 0) {
